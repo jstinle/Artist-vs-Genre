84e133db304250ef9dc7608be5d860a03c9da21b
krxxsten <krrxsten@uw.edu>
2023-05-30 15:07:07 -0700

    j

--- Dataset/clean_data.py
+++ Dataset/clean_data.py
@@ -1,9 +1,9 @@
-'''
+"""
 Name: Kristen, Justin, and Jayden 
 
 This file contains functions that will take in our data_w_spotify csv file and
 clean it up. We will be computing data, removing and adding columns. 
-'''
+"""
 
 import pandas as pd 
 import geopandas as gpd 
