ec551dfe3e2baa81a40c5da2b0229cb4bf80ec07
krxxsten <krrxsten@uw.edu>
2023-05-31 15:21:15 -0700

    kris!

--- Dataset/clean_data.py
+++ Dataset/clean_data.py
@@ -7,31 +7,51 @@ clean it up. We will be computing data, removing and adding columns.
 
 import pandas as pd
 
-artist_success_pd = pd.read_csv("/Users/krxxsten/Artist-vs-Genre-1/Dataset/data_w_spotify.csv")
+csv = ("/Users/krxxsten/Artist-vs-Genre-1/Dataset/data_w_spotify.csv")
+artist_pd = pd.read_csv(csv)
 
 
-def catergorized_genre(artist_success_pd: pd.DataFrame) -> None:
+def catergorized_genre(artist_pd: pd.DataFrame) -> None:
     '''
-    Takes in our dataframe and iterates through the list of genres of each artist name
-    and makes a new column with the first genre index of their list. 
+    Takes in our dataframe and iterates through the list of genres of each
+    artist name and makes a new column with the first genre index of their list.
     '''
-    
 
-    if "genres" in artist_success_pd.columns:
-        artist_success_pd["updated_genre"] = artist_success_pd["genres"].str.split(",").str[0]
-        artist_success_pd.dropna(subset=["updated_genre"], inplace=True)
-    artist_success_pd = artist_success_pd[artist_success_pd["updated_genre"] != []]
-   
-    return artist_success_pd
+    if "genres" in artist_pd.columns:
+        artist_pd["genres"] = artist_pd["genres"].str.strip("[]").str.replace("", "")
+        artist_pd["updated_genre"] = artist_pd["genres"].str.split(",").str[0]
+        artist_pd.dropna(subset=["updated_genre"], inplace=True)
+        
 
-print(catergorized_genre(artist_success_pd))
+    return artist_pd
 
 
-def drop_columns(artist_success_pd: pd.DataFrame) -> None:
+
+def drop_columns(artist_pd: pd.DataFrame) -> None:
     '''
     Takes in our dataframe and drop the following columns; "id", "isdone", and "spotifyid".
-    In order to narrow our dataframe to keeping data that supports our research questions, we
-    will be deleting those columns.
+    In order to narrow our dataframe to keeping data that supports our 
+    research questions, we will be deleting those columns.
     '''
-    artist_success_pd = artist_success_pd.drop(columns= ["id", "isdone", "spotifyid", "genres"], axis= 1)
-    return artist_success_pd
+    df2 = catergorized_genre(artist_pd)
+    artist_pd = df2.drop(columns=["id", "isdone", "spotifyid", "genres"], axis=1)
+    return artist_pd
+
+
+def transfer_to_csv(artist_pd: pd.DataFrame) -> None:
+    '''
+    Takes in our dataframe and combines the two data wrangling functions and
+    transfers it into a new csv file with the updated data.
+    '''
+    genre = catergorized_genre(artist_pd)
+    dropped = drop_columns(genre)
+
+    return dropped.to_csv('artist_success2.csv', index=False)
+
+
+def main():
+    transfer_to_csv(artist_pd)
+
+
+if __name__ == '__main__':
+    main()
