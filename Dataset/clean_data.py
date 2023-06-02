5ff53211120c116ed1a329df9e2f8e93b8e2b8d4
krxxsten <krrxsten@uw.edu>
2023-06-01 19:25:36 -0700

    kris!

--- Dataset/clean_data.py
+++ Dataset/clean_data.py
@@ -1,8 +1,8 @@
 """
-Name: Kristen, Justin, and Jayden 
+Name: Kristen, Justin, and Jayden
 
 This file contains functions that will take in our data_w_spotify csv file and
-clean it up. We will be computing data, removing and adding columns. 
+clean it up. We will be computing data, removing and adding columns.
 """
 
 import pandas as pd
@@ -14,28 +14,32 @@ artist_pd = pd.read_csv(csv)
 def catergorized_genre(artist_pd: pd.DataFrame) -> None:
     '''
     Takes in our dataframe and iterates through the list of genres of each
-    artist name and makes a new column with the first genre index of their list.
+    artist name and makes a new column with the first genre index of
+    their list.
     '''
 
     if "genres" in artist_pd.columns:
         artist_pd["genres"] = artist_pd["genres"].str.strip("[]").str.replace("", "")
         artist_pd["updated_genre"] = artist_pd["genres"].str.split(",").str[0]
         artist_pd.dropna(subset=["updated_genre"], inplace=True)
-        
 
     return artist_pd
 
 
-
 def drop_columns(artist_pd: pd.DataFrame) -> None:
     '''
-    Takes in our dataframe and drop the following columns; "id", "isdone", and "spotifyid".
-    In order to narrow our dataframe to keeping data that supports our 
-    research questions, we will be deleting those columns.
+    Takes in our dataframe and drop the following columns;
+    "id", "isdone", and "spotifyid". In order to narrow our dataframe
+    to keeping data that supports our research questions, we will be deleting
+    those columns.
     '''
-<<<<<<< HEAD
     df2 = catergorized_genre(artist_pd)
-    artist_pd = df2.drop(columns=["id", "isdone", "spotifyid", "genres"], axis=1)
+    artist_pd = df2.drop(columns=[
+                                  "id",
+                                  "isdone",
+                                  "spotifyid",
+                                  "genres"], axis=1)
+
     return artist_pd
 
 
@@ -56,7 +60,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
-    artist_success_pd = artist_success_pd.drop(columns= ["id", "isdone", "spotifyid", "genres"], axis= 1)
-    return artist_success_pd
->>>>>>> f936033e73700085694fb20f48b3b968bc0f555f
