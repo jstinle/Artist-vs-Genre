802ef1bf1e2b092a3c3fd0e037587dcab0bf6a00
krxxsten <krrxsten@uw.edu>
2023-06-01 19:32:25 -0700

    kris

--- Dataset/clean_data.py
+++ Dataset/clean_data.py
@@ -8,54 +8,54 @@ clean it up. We will be computing data, removing and adding columns.
 import pandas as pd
 
 csv = ("/Users/krxxsten/Artist-vs-Genre-1/Dataset/data_w_spotify.csv")
-artist_pd = pd.read_csv(csv)
+art_pd = pd.read_csv(csv)
 
 
-def catergorized_genre(artist_pd: pd.DataFrame) -> None:
+def catergorized_genre(art_pd: pd.DataFrame) -> None:
     '''
     Takes in our dataframe and iterates through the list of genres of each
     artist name and makes a new column with the first genre index of
     their list.
     '''
 
-    if "genres" in artist_pd.columns:
-        artist_pd["genres"] = artist_pd["genres"].str.strip("[]").str.replace("", "")
-        artist_pd["updated_genre"] = artist_pd["genres"].str.split(",").str[0]
-        artist_pd.dropna(subset=["updated_genre"], inplace=True)
+    if "genres" in art_pd.columns:
+        art_pd["genres"] = art_pd["genres"].str.strip("[]").str.replace("", "")
+        art_pd["updated_genre"] = art_pd["genres"].str.split(",").str[0]
+        art_pd.dropna(subset=["updated_genre"], inplace=True)
 
-    return artist_pd
+    return art_pd
 
 
-def drop_columns(artist_pd: pd.DataFrame) -> None:
+def drop_columns(art_pd: pd.DataFrame) -> None:
     '''
     Takes in our dataframe and drop the following columns;
     "id", "isdone", and "spotifyid". In order to narrow our dataframe
     to keeping data that supports our research questions, we will be deleting
     those columns.
     '''
-    df2 = catergorized_genre(artist_pd)
-    artist_pd = df2.drop(columns=[
+    df2 = catergorized_genre(art_pd)
+    art_pd = df2.drop(columns=[
                                   "id",
                                   "isdone",
                                   "spotifyid",
                                   "genres"], axis=1)
 
-    return artist_pd
+    return art_pd
 
 
-def transfer_to_csv(artist_pd: pd.DataFrame) -> None:
+def transfer_to_csv(art_pd: pd.DataFrame) -> None:
     '''
     Takes in our dataframe and combines the two data wrangling functions and
     transfers it into a new csv file with the updated data.
     '''
-    genre = catergorized_genre(artist_pd)
+    genre = catergorized_genre(art_pd)
     dropped = drop_columns(genre)
 
     return dropped.to_csv('artist_success2.csv', index=False)
 
 
 def main():
-    transfer_to_csv(artist_pd)
+    transfer_to_csv(art_pd)
 
 
 if __name__ == '__main__':
