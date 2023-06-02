e95f385b5ba1bfc5d7aef69a508f167ea4008eae
jstinle <jxle@uw.edu>
2023-06-01 20:20:56 -0700

    flake

--- Research/top_20_genre.py
+++ Research/top_20_genre.py
@@ -1,8 +1,9 @@
 '''
 Name: Justin Le
 
-This file contains functions that will be utilized to answer the following question
-With wide ranges of genres and subgenres nowadays, what specific genre(s) dominate the
-20% percentile of artists' popularity on Spotify based on our dataset?". 
+This file contains functions that will be utilized to answer the following
+question: "With wide ranges of genres and subgenres nowadays, what specific
+genre(s) dominate the 20% percentile of artists' popularity on Spotify based
+on our dataset?".
 This will be based off of our updated dataset, "artist_success2.csv".
 '''
